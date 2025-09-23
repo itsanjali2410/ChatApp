f083265eb198f93d7748dc8faadb8e8650d4e1ea
itsanjali2410 <anjalig9002@gmail.com>
2025-09-23 19:23:15 +0530

    everything is good

--- frontend/app/chat/page.tsx
+++ frontend/app/chat/page.tsx
@@ -1,5 +1,6 @@
-export const dynamic = 'force-dynamic';
 "use client";
+
+export const dynamic = 'force-dynamic';
 import React, { useEffect, useState, useRef } from "react";
 import api, { getFileUrl } from "../../utils/api";
 import { useWebSocket } from "../../hooks/useWebSocket";
